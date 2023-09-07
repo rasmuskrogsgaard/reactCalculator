9659be2bf6c57e7eb29965a6123f6b00a36b3761
Rasmus Krogsgaard <rasmus.krogsgaard@gmail.com>
2023-09-07 14:06:58 +0200

    Initial commit

---
+++ calculator/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Calculator from './components/calculator.jsx'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+    <Calculator></Calculator>
+
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
